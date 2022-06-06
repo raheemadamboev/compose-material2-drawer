5ac2a2bf9558659cbc5e4adff44bb7732c59382e
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-06 17:00:18 +0500

    implement drawer

--- app/src/main/java/xyz/teamgravity/composematerial2drawer/presentation/activity/Main.kt
+++ app/src/main/java/xyz/teamgravity/composematerial2drawer/presentation/activity/Main.kt
@@ -3,11 +3,24 @@ package xyz.teamgravity.composematerial2drawer.presentation.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.launch
+import xyz.teamgravity.composematerial2drawer.R
+import xyz.teamgravity.composematerial2drawer.presentation.component.AppBar
+import xyz.teamgravity.composematerial2drawer.presentation.component.DrawerContent
 import xyz.teamgravity.composematerial2drawer.presentation.theme.ComposeMaterial2DrawerTheme
+import xyz.teamgravity.composematerial2drawer.presentation.viewmodel.MainViewModel
 
 class Main : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +31,49 @@ class Main : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    val viewmodel = viewModel<MainViewModel>()
+                    val scaffold = rememberScaffoldState()
+                    val scope = rememberCoroutineScope()
+
+                    Scaffold(
+                        scaffoldState = scaffold,
+                        topBar = {
+                            AppBar(
+                                onNavigationClick = {
+                                    scope.launch { scaffold.drawerState.open() }
+                                }
+                            )
+                        },
+                        floatingActionButton = {
+                            FloatingActionButton(
+                                onClick = viewmodel::onHeartCheckedChange,
+                                backgroundColor = MaterialTheme.colors.primary
+                            ) {
+                                Icon(
+                                    imageVector = if (viewmodel.heartChecked) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                                    contentDescription = stringResource(id = R.string.cd_favorite_button),
+                                )
+                            }
+                        },
+                        drawerContent = {
+                            DrawerContent(
+                                menus = viewmodel.menus,
+                                onMenuClick = { menu ->
+                                    scope.launch { scaffold.drawerState.close() }
+                                    viewmodel.onSelectedMenuChange(menu)
+                                }
+                            )
+                        }
+                    ) { padding ->
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(padding)
+                        ) {
+                            Text(text = stringResource(id = viewmodel.selectedMenu.title))
+                        }
+                    }
                 }
             }
         }
